ba5726d066c99ed71b368274b6fd896d57d8ad72
Angel Serrano <angel.serranoguerales@gmail.com>
2021-02-22 19:55:21 +0100

    Added lab 7.2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { IntroComponent } from './intro/intro.component';
+import { NavComponent } from './nav/nav.component';
+import { FeaturedPostComponent } from './featured-post/featured-post.component';
+import { Article1Component } from './article1/article1.component';
+import { Article2Component } from './article2/article2.component';
+import { Article3Component } from './article3/article3.component';
+import { PaginationComponent } from './pagination/pagination.component';
+import { FooterComponent } from './footer/footer.component';
+import { CopyrightComponent } from './copyright/copyright.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    IntroComponent,
+    NavComponent,
+    FeaturedPostComponent,
+    Article1Component,
+    Article2Component,
+    Article3Component,
+    PaginationComponent,
+    FooterComponent,
+    FooterComponent,
+    CopyrightComponent
   ],
   imports: [
     BrowserModule
